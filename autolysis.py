703ef0425504bd674a94ca947735b176a7fd22a5
Samcoderg78 <155004607+Samcoderg78@users.noreply.github.com>
2024-12-14 18:05:41 +0530

    Update autolysis.py

--- autolysis.py
+++ autolysis.py
@@ -1,3 +1,14 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+# "httpx",
+# "pandas",
+# "matplotlib",
+# "requests",
+# "seaborn",
+# "charset_normalizer",
+# "chardet",
+# ]
 import os
 import sys
 import pandas as pd
